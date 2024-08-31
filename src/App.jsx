45cae36b73c2c9b02df9043993617ec7fedb36af
samish24 <samishaddad@protonmail.com>
2024-08-31 18:25:00 +0200

    adding cards, updating handleSubmit function, fixing bugs, add styling, hoping it's the last commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import DiaryList from './Components/DiaryList';
-import AddEntryModal from './Components/AddEntryModal';
-import EntryModal from './Components/EntryModal';
+import EntryForm from "./components/EntryForm";
+import EntryList from "./components/EntryList.jsx";
 
 const App = () => {
-  const [entries, setEntries] = useState([]);
-  const [selectedEntry, setSelectedEntry] = useState(null);
-  const [showAddEntryModal, setShowAddEntryModal] = useState(false);
-  const [showEntryModal, setShowEntryModal] = useState(false);
-
-  // Load entries from localStorage on mount
-  useEffect(() => {
-    const storedEntries =
-      JSON.parse(localStorage.getItem('diaryEntries')) || [];
-    setEntries(storedEntries);
-  }, []);
-
-  // Function to add entry
-  const addEntry = (newEntry) => {
-    const updatedEntries = [newEntry, ...entries];
-    setEntries(updatedEntries);
-    localStorage.setItem('diaryEntries', JSON.stringify(updatedEntries));
-  };
-
   return (
-    <div className='app-container'>
-      <button onClick={() => setShowAddEntryModal(true)}>Add Entry</button>
-      <DiaryList
-        entries={entries}
-        onEntryClick={(entry) => {
-          setSelectedEntry(entry);
-          setShowEntryModal(true);
-        }}
-      />
-      {showAddEntryModal && (
-        <AddEntryModal
-          onClose={() => setShowAddEntryModal(false)}
-          onAddEntry={addEntry}
-        />
-      )}
-      {showEntryModal && (
-        <EntryModal
-          entry={selectedEntry}
-          onClose={() => setShowEntryModal(false)}
-        />
-      )}
-    </div>
+    <>
+      <h1>You Personal Diary</h1>
+      <EntryForm />
+      <EntryList />
+    </>
   );
 };
 export default App;
---
+++ src/components/EntryForm.jsx
@@ -0,0 +1,74 @@
+import { useState } from "react";
+
+const initialEntryForm = {
+  title: "",
+  date: "",
+  imageUrl: "",
+  content: "",
+};
+
+const EntryForm = () => {
+  const [formState, setFormState] = useState(initialEntryForm);
+
+  const handleChange = (e) => {
+    setFormState((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const existingEntries = JSON.parse(localStorage.getItem("formData")) || [];
+    const isEntryForToday = existingEntries.some(
+      (entry) => entry.date === formState.date
+    );
+    if (isEntryForToday) {
+      alert("An entry already exists for today. Please return the next day.");
+      return;
+    }
+    const updatedEntries = [...existingEntries, formState];
+    localStorage.setItem("formData", JSON.stringify(updatedEntries));
+    setFormState(initialEntryForm);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="contact-form">
+      <h2 className="text-3xl">Add your Entry</h2>
+      <label htmlFor="name">Titel</label>
+      <input
+        type="text"
+        name="title"
+        id="title"
+        value={formState.title}
+        onChange={handleChange}
+      />
+      <label htmlFor="date">Date</label>
+      <input
+        type="date"
+        name="date"
+        id="date"
+        value={formState.date}
+        onChange={handleChange}
+      />
+
+      <label htmlFor="imageUrl">Image URL</label>
+      <input
+        type="url"
+        name="imageUrl"
+        id="imageUrl"
+        value={formState.imageUrl}
+        onChange={handleChange}
+      />
+
+      <label htmlFor="message">Content</label>
+      <textarea
+        name="content"
+        id="content"
+        value={formState.content}
+        onChange={handleChange}
+      ></textarea>
+      <button>Submit</button>
+    </form>
+  );
+};
+
+export default EntryForm;
