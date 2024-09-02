d02b2020dcbbdd54463f7c7fb8527936a3623681
GitLernya <174644721+Lernya@users.noreply.github.com>
2024-09-02 06:41:49 +0200

    Das Styling

--- src/components/EntryList.jsx
+++ src/components/EntryList.jsx
@@ -26,7 +26,7 @@ const EntryList = () => {
   };
 
   return (
-    <div>
+    <div id="entrylist">
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
         {entries.map((entry, index) => (
           <div
