6939813c9eadcfbba0b172c45dd9177a60a23a94
GitLernya <174644721+Lernya@users.noreply.github.com>
2024-09-02 09:41:30 +0200

    CSS mit hoeherer Spezifizitaet

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import EntryList from "./components/EntryList.jsx";
 const App = () => {
   return (
     <>
-      <h1>Personal Diary</h1>
+      <h1 id="h1app">Personal Diary</h1>
       <EntryForm />
       <EntryList />
     </>
